d80eac9cc41fd2c45880449690e4c778b283ba0b
gavimar <gavimar182@gmail.com>
2020-04-21 20:26:44 +0200

    fetch ok2

--- src/components/RouteList.js
+++ src/components/RouteList.js
@@ -17,21 +17,13 @@ const RouteList = (props) => {
 
 }, [props.search]);
 
-//  const fetchClosest = () => {
-//   fetch(`https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=40.03&lon=-105.25&maxDistance=10&minDiff=5.6&maxDiff=5.10&key=200719178-8e0de0f7ec53dfe8e72e54c34f99e721`)
-//       .then(res => res.json())
-//       .then(data => setData(data.routes))
-//       .catch(() => setErrors(true));
-//       console.log(data);
-//  }
 
 
 
 return(
   
   <ul>
-    {/* <div>{JSON.stringify(data)}</div>; */}
-    {/* <button type="button" className="closestButton" onClick={fetchClosest}>Get closest</button> */}
+   
     
   {data&&data.map((routeObj) => 
   
