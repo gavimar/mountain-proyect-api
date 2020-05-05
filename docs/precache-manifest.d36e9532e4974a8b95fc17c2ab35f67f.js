360d4dc3fcf5a3cc59ee17681bd04e3e71b10287
gavimar <gavimar182@gmail.com>
2020-05-05 22:38:00 +0200

    docs

---
+++ docs/precache-manifest.d36e9532e4974a8b95fc17c2ab35f67f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6289930c6e42a9766e3226ceb7a029c9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c8ca83c9ba367ecc8214",
+    "url": "./static/css/2.6f3a26c5.chunk.css"
+  },
+  {
+    "revision": "494ffd35998e43e3ec77",
+    "url": "./static/css/main.c142d339.chunk.css"
+  },
+  {
+    "revision": "c8ca83c9ba367ecc8214",
+    "url": "./static/js/2.bb83a643.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "./static/js/2.bb83a643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "494ffd35998e43e3ec77",
+    "url": "./static/js/main.d6b6bca0.chunk.js"
+  },
+  {
+    "revision": "81b8d40316d08e8aed37",
+    "url": "./static/js/runtime-main.9ff822f9.js"
+  },
+  {
+    "revision": "80f97ee4b157854f3d7c94c2b701f41f",
+    "url": "./static/media/header3.80f97ee4.jpg"
+  }
+]);
